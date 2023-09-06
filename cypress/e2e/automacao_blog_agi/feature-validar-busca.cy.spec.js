575858c4e1d43de7b6bb9ddfede8ac5e239e1496
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2023-09-06 12:20:04 -0300

    update: testes de busca

---
+++ cypress/e2e/automacao_blog_agi/feature-validar-busca.cy.spec.js
@@ -0,0 +1,24 @@
+// busca.spec.js
+import elementos from './elementos'; // Importe os seletores de elementos
+
+describe('Busca na página Home', () => {
+  context('Validar busca na página home', () => {
+    beforeEach(() => {
+      cy.visit('https://blogdoagi.com.br');
+    });
+
+    it('Validar busca por pesquisa existente', () => {
+      cy.get(elementos.bt_search).click();
+      cy.get(elementos.input_search).type('Teste');
+      cy.get(elementos.bt_submit).click();
+      cy.get(elementos.result_true).should('contain', 'Teste'); // Usar should para espera explícita
+    });
+
+    it('Validar busca por pesquisa inexistente', () => {
+      cy.get(elementos.bt_search).click();
+      cy.get(elementos.input_search).type('xxx123');
+      cy.get(elementos.bt_submit).click();
+      cy.get(elementos.result_false).should('contain', 'Nenhum resultado'); // Usar should para espera explícita
+    });
+  });
+});
